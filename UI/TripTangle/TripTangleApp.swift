db3949b23942ef5a4d26ac8bffa3fb1edf2cea6b
iAboud98 <aboudfialah98@gmail.com>
2025-05-03 13:08:24 +0200

    set up Swift project

---
+++ UI/TripTangle/TripTangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripTangleApp.swift
+//  TripTangle
+//
+//  Created by Aboud Fialah on 03/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TripTangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UI/TripTangleTests/TripTangleTests.swift
@@ -0,0 +1,17 @@
+//
+//  TripTangleTests.swift
+//  TripTangleTests
+//
+//  Created by Aboud Fialah on 03/05/2025.
+//
+
+import Testing
+@testable import TripTangle
+
+struct TripTangleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
